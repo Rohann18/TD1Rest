f38ff63fb096c6b00ee7dd9ea1142d0ec982d5da
Rohann18 <rohann.bella@hotmail.fr>
2022-05-30 14:21:32 +0200

    Fin modifs EtudiantController

--- src/main/java/com/inti/TD1Rest/controller/EtudiantController.java
+++ src/main/java/com/inti/TD1Rest/controller/EtudiantController.java
@@ -8,11 +8,13 @@ import com.inti.TD1Rest.repository.EtudiantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -42,9 +44,9 @@ public class EtudiantController {
 		return "L'Etudiant a bien été modifié.";
 	}
 	
-	@PostMapping("/delete")
-	public String deleteEtudiant() {
-		etudiantRepository.deleteById(1);
-		return "L'Etudiant a bien été supprimé.";
+	@DeleteMapping("/delete")
+	public String deleteEtudiant(@RequestParam("id") int id) {
+		etudiantRepository.deleteById(id);
+		return "Etudiant supprimé.";
 	}
 }
